e7127b82dae2191ea0f3a1d875e598cb11e3a873
Riley Burke <rileyburke0@hotmail.com>
2022-04-06 21:45:34 -0230

    Got the json from the api to work.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict"
 
-$("#weather").weatherForecast({
+$("#open_weather").weatherForecast({
     openWeatherApiKey: "adcf237fa5811f92e8a66288993e9335"
 });
+
--- jquery.weather-forecast-rb.js
+++ jquery.weather-forecast-rb.js
@@ -3,14 +3,45 @@
 
         let settings = $.extend({
             openWeatherApiKey: null,
-            geoLocation: 0
+            geoLocation: 0,
+            iconSet: 0,
+            closeable: true
         }, options);
 
+
+
         function getWeatherInfo(city_name, mode) {
-            const api_key = settings.apiKey;
-            const api_url = "https://api.openweathermap.org/data/2.5/" + mode + "?q=" + city_name + "&appid=" + api_key + "&units=metric"
+            const api_key = settings.openWeatherApiKey;
+            const api_url = "https://api.openweathermap.org/data/2.5/" + mode + "?q=" + city_name + "&appid=" + api_key + "&units=metric";
+            console.log(api_url);
+            fetch(api_url)
+                .then(response => response.json())
+                .then( json => console.log(json) )
+                .catch(e => console.log(e.message));
         }
 
+
+        return this.each( () => {
+            console.log("hello");
+            let $display, $icon;
+            // setWeatherDisplayProperties();
+            // setWeatherIconProperties();
+            getWeatherInfo("Halifax", "weather");
+
+
+            $(this).find('a').on("click", function(event) {
+                console.log("test");
+                event.preventDefault();
+            });
+            //
+            // function setWeatherDisplayProperties() {
+            //
+            // }
+            //
+            // function setWeatherIconProperties() {
+            //
+            // }
+        });
 
         //This will require use of another api. Add later as an option for users.
         // function getLocation(){
